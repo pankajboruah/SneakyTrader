a06c80f120373422ae0efb5dd87d178d293ce893
Pankaj Boruah <pankaj.boruah@upgrad.com>
2021-12-31 18:11:45 +0530

    added scss+antd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import { Card, Input } from "antd";
+import './sass/index.scss';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header >
+        <Card title="card">
+          Scaffold
+          <Input placeholder="enter"/>
+        </Card>
       </header>
     </div>
   );
